fdbb6adad140c00ff298071055eed72b9ca41b04
Can A. MOGOL <canmogol@gmail.com>
2014-10-22 10:29:56 +0300

    nullpointer fixed

--- src/com/fererlab/map/MessageProperties.java
+++ src/com/fererlab/map/MessageProperties.java
@@ -39,17 +39,20 @@ public class MessageProperties extends HashMap<String, Map<String, String>> {
             URL url = getClass().getClassLoader().getResource(directoryName);
             if (url != null) {
                 File directory = new File(url.getPath());
-                if (directory != null && directory.isDirectory() && directory.listFiles() != null) {
-                    for (File file : directory.listFiles()) {
-                        if (file != null && file.getName().startsWith("message_") && file.getName().endsWith(".properties")) {
-                            String locale = file.getName().substring("message_".length(), file.getName().length() - ".properties".length());
-                            if (!containsKey(locale)) {
-                                put(locale, new HashMap<String, String>());
-                            }
-                            Properties properties = new Properties();
-                            properties.load(new FileReader(file));
-                            for (Object key : properties.keySet()) {
-                                get(locale).put(String.valueOf(key), String.valueOf(properties.get(key)));
+                if (directory.exists() && directory.isDirectory()) {
+                    File[] files = directory.listFiles();
+                    if (files != null) {
+                        for (File file : files) {
+                            if (file != null && file.getName().startsWith("message_") && file.getName().endsWith(".properties")) {
+                                String locale = file.getName().substring("message_".length(), file.getName().length() - ".properties".length());
+                                if (!containsKey(locale)) {
+                                    put(locale, new HashMap<String, String>());
+                                }
+                                Properties properties = new Properties();
+                                properties.load(new FileReader(file));
+                                for (Object key : properties.keySet()) {
+                                    get(locale).put(String.valueOf(key), String.valueOf(properties.get(key)));
+                                }
                             }
                         }
                     }
